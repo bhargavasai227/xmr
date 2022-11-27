cc60805fbc5f4be7a136b905419090938151be2e
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2022-11-27 14:21:43 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  setTimeout(() => {
+    update();
+  }, 1000);
+  const [price, setprice] = useState([]);
+  function update() {
+    axios
+      .get(
+        "https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=XMR,INR"
+      )
+      .then((responce) => {
+        setprice(responce.data);
+      });
+  }
+  let priceINR = (0.021639733645 * price.INR - 299.92).toFixed(4);
+
+  return (
+    <div className="container">
+      <h1 style={{ color: priceINR > 0 ? "green" : "red", fontSize: "300%" }}>
+        YourP/L:{priceINR}
+      </h1>
+      <h4>one coin: {price.INR}</h4>
+    </div>
+  );
+}
+export default App;
